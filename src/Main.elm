a14c979eca7b3f9af4b5503434416032b6747b46
Borislav Grigorov <brslvg@gmail.com>
2018-09-11 20:19:20 +0300

    Use elm-format to format the code

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,6 @@
 {--
 NOTES
 
-Next...define what's happening in the update function...
-
 [x] make the game layout first
   [x] add title
   [x] style the field
@@ -11,99 +9,128 @@ Next...define what's happening in the update function...
 [ ] define the game's update func
 --}
 
-module Main exposing (..)
+
+module Main exposing (Box, Field, Model, Msg(..), Player(..), init, initialState, main, n, p1, p2, update, view)
 
 import Browser
-import Html exposing (div, text, h1)
+import Html exposing (div, h1, text)
 import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
 
+
+
 -- MAIN
 
-main =
-  Browser.sandbox
-    { init = init
-    , view = view
-    , update = update
-    }
 
-init = initialState
+main =
+    Browser.sandbox
+        { init = init
+        , view = view
+        , update = update
+        }
+
+
+init =
+    initialState
+
 
 
 -- MODEL
 
-type alias Box =
-  (Int, Int)
 
-type Player =
-  P1 String
-  | P2 String
-  | None String
+type alias Box =
+    ( Int, Int )
+
+
+type Player
+    = P1 String
+    | P2 String
+    | None String
+
 
 type alias Field =
-  List (List Player) -- Matrix
+    List (List Player)
 
-type alias Model = 
-  { field : Field
-  , currentPlayer : Player
-  }
+
+
+-- Matrix
+
+
+type alias Model =
+    { field : Field
+    , currentPlayer : Player
+    }
+
 
 p1 : Player
 p1 =
-  P1 "X"
+    P1 "X"
+
 
 p2 : Player
 p2 =
-  P2 "O"
+    P2 "O"
+
 
 n : Player
 n =
-  None ""
+    None ""
+
 
 initialState =
-  { field = [ [ n, n, n ]
-            , [ n, n, n ]
-            , [ n, n, n ]
-            ]
-  , currentPlayer = p1
-  }
+    { field =
+        [ [ n, n, n ]
+        , [ n, n, n ]
+        , [ n, n, n ]
+        ]
+    , currentPlayer = p1
+    }
+
+
 
 -- UPDATE
 
-type Msg =
-  Turn Box -- Each player's turn will carry information about the box
+
+type Msg
+    = Turn Box -- Each player's turn will carry information about the box
+
 
 update msg model =
-  let
-    _ =
-      Debug.log "msg" msg -- we still don't enter here, because no updates are invoked...
-  in
+    let
+        _ =
+            Debug.log "msg" msg
+
+        -- we still don't enter here, because no updates are invoked...
+    in
     case msg of
-      Turn box ->
-        model
+        Turn box ->
+            model
+
+
 
 -- VIEW
 
+
 view model =
-  div [ class "game-wrapper" ]
-      [ h1 [ class "title" ]
-         [ text "Tic Tac Toe"
-         ]
-      , div [ class "field" ]
-            [ div [ class "row" ]
-                  [ div [ class "box", onClick (Turn (0, 0)) ] []
-                  , div [ class "box", onClick (Turn (0, 1)) ] []
-                  , div [ class "box", onClick (Turn (0, 2)) ] []
-                  ]
-            , div [ class "row" ]
-                  [ div [ class "box" ] []
-                  , div [ class "box" ] []
-                  , div [ class "box" ] []
-                  ]
-            , div [ class "row" ]
-                  [ div [ class "box" ] []
-                  , div [ class "box" ] []
-                  , div [ class "box" ] []
-                  ]
+    div [ class "game-wrapper" ]
+        [ h1 [ class "title" ]
+            [ text "Tic Tac Toe"
             ]
-      ]
+        , div [ class "field" ]
+            [ div [ class "row" ]
+                [ div [ class "box", onClick (Turn ( 0, 0 )) ] []
+                , div [ class "box", onClick (Turn ( 0, 1 )) ] []
+                , div [ class "box", onClick (Turn ( 0, 2 )) ] []
+                ]
+            , div [ class "row" ]
+                [ div [ class "box" ] []
+                , div [ class "box" ] []
+                , div [ class "box" ] []
+                ]
+            , div [ class "row" ]
+                [ div [ class "box" ] []
+                , div [ class "box" ] []
+                , div [ class "box" ] []
+                ]
+            ]
+        ]
